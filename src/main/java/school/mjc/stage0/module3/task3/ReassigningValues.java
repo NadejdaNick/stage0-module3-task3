743cdd8d5f7036ca9a579c9bb0b8cf2a9abc9393
NadejdaNick <nhohlova2021@gmail.com>
2023-09-16 17:34:53 +0300

    changed

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -3,5 +3,11 @@ package school.mjc.stage0.module3.task3;
 public class ReassigningValues {
     public static void main(String[] args) {
         //write code here
+        int first = 1, second = 10, third = 100;
+        int linkToFirst, linkToSecond, linkToThird;
+        first = 15;
+        second = 6;
+        third = 4;
+        System.out.println(first + "\n" + second + "\n" + third);
     }
 }
